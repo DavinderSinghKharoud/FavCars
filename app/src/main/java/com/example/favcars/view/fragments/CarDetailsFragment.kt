fff3b44ae0d2d124f50047c112c6d0db5b73ad53
DavinderSinghKharoud <dskharoud2@gmail.com>
2021-09-12 20:57:00 -0400

    Palette

--- app/src/main/java/com/example/favcars/view/fragments/CarDetailsFragment.kt
+++ app/src/main/java/com/example/favcars/view/fragments/CarDetailsFragment.kt
@@ -1,12 +1,20 @@
 package com.example.favcars.view.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.favcars.databinding.FragmentCarDetailsBinding
 import java.util.*
 
@@ -30,12 +38,49 @@ class CarDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //get the arguments
         val args: CarDetailsFragmentArgs by navArgs()
+        bindData(args)
+    }
+
+    /**
+     * Bind the data
+     */
+    private fun bindData(args: CarDetailsFragmentArgs) {
         args.let {
             try {
                 Glide.with(requireActivity())
                     .load(it.carDetails.image)
                     .centerCrop()
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            Log.e(TAG, "Error loading image", e)
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            //use palette to set the background
+                            resource.let {
+                                Palette.from(resource!!.toBitmap()).generate() { palette ->
+                                    val intColor = palette?.vibrantSwatch?.rgb ?: 0
+                                    mBinding!!.rlCarDetails.setBackgroundColor(intColor)
+                                }
+                            }
+                            return false
+                        }
+
+                    })
                     .into(mBinding!!.ivCarImage)
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -45,10 +90,10 @@ class CarDetailsFragment : Fragment() {
             mBinding!!.tvEnginePower.text = it.carDetails.enginePow
             mBinding!!.tvPriceRange.text = it.carDetails.priceRange
             mBinding!!.tvDescription.text = it.carDetails.description
-            if(mBinding!!.tvReview.text.isNotEmpty()){
+            if (mBinding!!.tvReview.text.isNotEmpty()) {
                 mBinding!!.tvTitleReview.visibility = View.VISIBLE
                 mBinding!!.tvReview.text = it.carDetails.review
-            }else  mBinding!!.tvTitleReview.visibility = View.GONE
+            } else mBinding!!.tvTitleReview.visibility = View.GONE
 
         }
     }
