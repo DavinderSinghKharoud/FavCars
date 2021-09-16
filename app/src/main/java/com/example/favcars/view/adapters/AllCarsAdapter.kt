e69bafcabf79eff226492430b6354d300d8d6704
DavinderSinghKharoud <dskharoud2@gmail.com>
2021-09-15 21:20:45 -0400

    Set up menu for an items

--- app/src/main/java/com/example/favcars/view/adapters/AllCarsAdapter.kt
+++ app/src/main/java/com/example/favcars/view/adapters/AllCarsAdapter.kt
@@ -1,10 +1,14 @@
 package com.example.favcars.view.adapters
 
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.favcars.R
 import com.example.favcars.databinding.ItemCarLayoutBinding
 import com.example.favcars.model.entities.Car
 import com.example.favcars.view.fragments.AllCarsFragment
@@ -18,7 +22,7 @@ class AllCarsAdapter(private val fragment: Fragment) :
     class ViewHolder(view: ItemCarLayoutBinding) : RecyclerView.ViewHolder(view.root) {
         val mDishImage = view.ivCarImage
         val tvTitle = view.tvCarName
-
+        val imMenu = view.imMenu
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -42,8 +46,32 @@ class AllCarsAdapter(private val fragment: Fragment) :
                 fragment.navigateCarDetails(car)
             }
         }
+        holder.imMenu.setOnClickListener{
+            //Set up popup menu
+           setUpPopUpMenu(holder)
+        }
+
+        if( fragment is AllCarsFragment){
+            holder.imMenu.visibility = View.VISIBLE
+        }else if (fragment is FavoriteCarsFragment){
+            holder.imMenu.visibility = View.GONE
+        }
     }
 
+    fun setUpPopUpMenu(holder: ViewHolder){
+        val popupMenu = PopupMenu(fragment.context, holder.imMenu)
+        popupMenu.menuInflater.inflate(R.menu.menu_adapter, popupMenu.menu)
+
+        popupMenu.setOnMenuItemClickListener {
+            if(it.itemId == R.id.edit_car){
+                Log.i("Edit", "")
+            }else if( it.itemId == R.id.delete_car){
+                Log.i("Delete", "")
+            }
+            true
+        }
+        popupMenu.show()
+    }
     override fun getItemCount(): Int {
         return cars.size
     }
